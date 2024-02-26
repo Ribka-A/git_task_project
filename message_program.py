92d198d90e80868878b523d4683e7a75c7b5b594
Ribka-A <111660290+Ribka-A@users.noreply.github.com>
2024-02-26 19:25:49 +0000

    Program asks user to select what the weather is like.

--- message_program.py
+++ message_program.py
@@ -1,3 +1,25 @@
 # print a message
-input_message = input("Please input a message to print: ")
-print(input_message)
+while True:
+    input_message = input('''\nPlease select what the weather is like today: 
+    A. Sunny
+    B. Rainy
+    C. Clear
+    D. [Exit program]
+    \nselection: ''').upper()
+
+    if input_message == "A":
+        print("\nIt is sunny outside!")
+    elif input_message == "B":
+        print("\nIt is rainy outside.")
+    elif input_message == "C":
+        print("\nThe sky is clear today.")
+    elif input_message == "D":
+        print("\n...Program exited.")
+        exit()
+    else:
+        print("\nPlease select a valid menu option from A-D.")
+
+
+
+
+
